d466eaed3651784210e3fd136085f5cf47a2f48f
brwngda <grz.barwinski@gmail.com>
2023-01-29 16:19:39 +0100

    nbłąd przy metodzie getAllCarManufacturers

--- src/app/page-car-list/page-car-list.component.ts
+++ src/app/page-car-list/page-car-list.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {CarService} from "../car-service/car.service";
+import {CarManufacturer} from "../model/car";
 
 @Component({
   selector: 'app-page-car-list',
@@ -7,6 +8,11 @@ import {CarService} from "../car-service/car.service";
   styleUrls: ['./page-car-list.component.css']
 })
 export class PageCarListComponent {
+
+  getAllCarManufacturers(): CarManufacturer[] {
+    return Object.keys(CarManufacturer)
+
+  }
 
   // kolumny które chcemy wyświetlić
   displayedColumns: string[] = [
